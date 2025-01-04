5883214a267c6ecbe1a27a014e592a68017dfb4b
FAZAL WARIS SIDDIQUI <fazalwaris799@gmail.com>
2025-01-04 21:36:20 +0500

    inital commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+export const routes: Routes = [
+    {
+        path:'',
+        component:HomeComponent,
+        pathMatch:"full"
+    },
+    {
+        path:'about',
+        component:AboutComponent
+    }
+];
