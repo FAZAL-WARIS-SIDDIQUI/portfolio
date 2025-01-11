cd76723fc3d83c30ecb564d56943770d9c8ed813
FAZAL WARIS SIDDIQUI <fazalwaris799@gmail.com>
2025-01-12 03:42:13 +0500

    css global styles

--- src/app/component/about/about.component.ts
+++ src/app/component/about/about.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: '../../app.component.css'
+  styleUrl: '../../app.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class AboutComponent {
 
--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { AboutComponent } from "../about/about.component";
 @Component({
   selector: 'app-home',
   imports: [AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: '../../app.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent {
 }
